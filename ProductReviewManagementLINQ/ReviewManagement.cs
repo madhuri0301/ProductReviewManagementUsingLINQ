435ea3bd1a34180d402c7a7e28e1f0916f7c8727
Madhuri <harshupatil0301@gmail>
2021-07-11 14:11:24 +0530

    [Madhuri]Add : Retriving The Given Data From The List

--- ProductReviewManagementLINQ/ReviewManagement.cs
+++ ProductReviewManagementLINQ/ReviewManagement.cs
@@ -20,5 +20,17 @@ namespace ProductReviewManagementLINQ
                     + " " + "Rating:- " + list.Rating + " " + "Review:- " + list.Review + " " + "isLike:- " + list.isLike);
             }
         }
+        public void SelectedRecords(List<ProductReview> listProductReview)
+        {
+            var recordedData = from productReviews in listProductReview
+                               where (productReviews.Product_ID == 1 || productReviews.Product_ID == 4 || productReviews.Product_ID == 9)
+                               && productReviews.Rating > 3
+                               select productReviews;
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("ProductID:- " + list.Product_ID + " " + "UserID:- " + list.User_ID
+                    + " " + "Rating:- " + list.Rating + " " + "Review:- " + list.Review + " " + "isLike:- " + list.isLike);
+            }
+        }
     }
 }
