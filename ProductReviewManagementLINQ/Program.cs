7208b80d8834559b1d702f029b8e7dcd4e26cfcf
Madhuri <harshupatil0301@gmail>
2021-07-11 13:55:14 +0530

    [Madhuri]Add : Retriving Top Three Records From List

--- ProductReviewManagementLINQ/Program.cs
+++ ProductReviewManagementLINQ/Program.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 
 namespace ProductReviewManagementLINQ
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args) //Main Method
         {
             Console.WriteLine("Welcome To The Product Review Management Program");
 
             //List : A List Class Can Be Used To Create A Collection Of A Any Type
-            List<ProductReview> productReviewList = new List<ProductReview>() 
+            List<ProductReview> productReviewList = new List<ProductReview>()
             {
                 new ProductReview(){Product_ID=1,User_ID=1,Rating=2,Review="Good",isLike=true},
                 new ProductReview(){Product_ID=2,User_ID=1,Rating=4,Review="Good",isLike=true},
@@ -24,11 +24,8 @@ namespace ProductReviewManagementLINQ
                 new ProductReview(){Product_ID=10,User_ID=1,Rating=8,Review="nice",isLike=true},
                 new ProductReview(){Product_ID=11,User_ID=1,Rating=3,Review="nice",isLike=true}
             };
-            foreach (var list in productReviewList) //Executing Each Element Present In List
-            {
-                Console.WriteLine("Product ID:- " + list.Product_ID + " " + "User ID:- " + list.User_ID
-                    + " " + "Rating:- " + list.Rating + " " + "Review:- " + list.Review + " " + "isLike:- " + list.isLike);
-            }
+            ReviewManagement management = new ReviewManagement();
+            management.TopRecords(productReviewList);
         }
     }
 }
---
+++ ProductReviewManagementLINQ/ReviewManagement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace ProductReviewManagementLINQ
+{
+    public class ReviewManagement
+    {
+        public readonly DataTable dataTable = new DataTable();
+        public void TopRecords(List<ProductReview> listProductReview)
+        {
+            var recordedData = (from productReviews in listProductReview
+                                orderby productReviews.Rating descending
+                                select productReviews).Take(3);
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("Product ID:- " + list.Product_ID + " " + "User ID:- " + list.User_ID
+                    + " " + "Rating:- " + list.Rating + " " + "Review:- " + list.Review + " " + "isLike:- " + list.isLike);
+            }
+        }
+    }
+}
